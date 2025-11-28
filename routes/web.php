c33b04244ce299eaca544f3778e490cbd33d0b7d
Gabriel Oliveira <gabriel@umobi.com.br>
2025-11-27 21:02:42 -0300

    feat: atualizar rotas para jurisprudência e adicionar endpoint para script.js

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,21 @@
 <?php
 
+use App\Http\Controllers\JurisprudenciaController;
 use Illuminate\Support\Facades\Route;
 
-Route::redirect('/', '/admin')->name('home');
+Route::redirect('/', '/jurisprudencia')->name('home');
+
+Route::get('/js/script.js', function () {
+    $path = public_path('js/script.js');
+
+    abort_unless(file_exists($path), 404);
+
+    return response()->file($path, [
+        'Content-Type' => 'application/javascript; charset=UTF-8',
+        'Cache-Control' => 'public, max-age=86400',
+    ]);
+})->name('assets.script');
+
+Route::get('/jurisprudencia', [JurisprudenciaController::class, 'index'])->name('jurisprudencia.index');
+Route::get('/jurisprudencia/api/search', [JurisprudenciaController::class, 'search'])->name('jurisprudencia.search');
+Route::get('/jurisprudencia/processos/{id}', [JurisprudenciaController::class, 'show'])->name('jurisprudencia.show');
